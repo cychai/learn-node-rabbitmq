b06275a2507aee53bcbca05b9945e665a6acb5fd
chaichunyan <chaichunyan@kingsoft.com>
2016-03-08 16:07:44 +0800

    update frontend info queue

--- openstack/frontend.queue.js
+++ openstack/frontend.queue.js
@@ -13,20 +13,21 @@ amqp.connect('amqp://ksyun:1qaz@WSX@10.0.5.156', function(err, conn) {
       var secs = msg.content.toString().split('.').length - 1;
 
       // console.log(" [x] Received %s", msg.content.toString());
-
-      // handle message
+      
       try{
+        // handle message
         console.log(JSON.parse(msg.content.toString()));
+        var msgContent = JSON.parse(msg.content.toString());
+      
+        var vm_id = msgContent.payload.instance_id,
+            vm_state = msgContent.payload.state,
+            timestamp = msgContent.timestamp;
+
+        console.log("[vmid:]%s  [state:]%s [timestamp:]%s", vm_id, vm_state, timestamp);
+
       }catch(e){
         console.log(e.name + ": " + e.message);
       }
-      var msgContent = JSON.parse(msg.content.toString());
-      
-      var vm_id = msgContent.payload.instance_id,
-          vm_state = msgContent.payload.state,
-          timestamp = msgContent.timestamp;
-
-      console.log("vm %s : state is : %s at %s", vm_id, vm_state, timestamp);
 
       setTimeout(function() {
         console.log(" [x] Done");
